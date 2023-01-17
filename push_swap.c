b0fe8741a94aeedb6c2844b5dc932bbef5d07d22
aoudija <amineoudija02@yahoo.com>
2023-01-17 13:53:57 +0100

    cmt2

--- push_swap.c
+++ push_swap.c
@@ -6,29 +6,51 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 07:52:48 by aoudija           #+#    #+#             */
-/*   Updated: 2023/01/17 10:10:38 by aoudija          ###   ########.fr       */
+/*   Updated: 2023/01/17 13:52:55 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void	sa(t_stack_a *head)
+void	sa(t_stack_a **head)
 {
-	t_stack_a	*temp;
+	int			temp;
 
-	temp = head->next;
-	head->next = head;
-	head = temp;
-	while (head)
+	temp = (*head)->num;
+	(*head)->num = (*head)->next->num;
+	(*head)->next->num = temp;
+	while ((*head))
 	{
-		printf("%d\n", head->num);
-		head = head->next;
+		printf("%d\n", (*head)->num);
+		(*head) = (*head)->next;
 	}
+}
+
+void	ra(t_stack_a **head)
+{
+	int			temp;
+	t_stack_a	*tempo;
+
+	tempo = (*head)->next;
+	temp = (*head)->num;
+	while (tempo)
+	{
+		if (!tempo->next)
+			break ;
+		tempo = tempo->next;
+	}
+	printf("%d\n", tempo->num);
+	(*head)->num = tempo->num;
+	// temp = (*head)->num;
+	// (*head)->num = (*head)->next->num;
+	// (*head)->next->num = temp;
+	// printf("%d\n", (*head)->num);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_stack_a	*head;
+	t_stack_a	*tempo;
 
 	head = ft_lstnew(ft_atoi(argv[argc - 1]));
 	argc -= 2;
@@ -37,5 +59,14 @@ int	main(int argc, char *argv[])
 		ft_lstadd_front(&head, ft_lstnew(ft_atoi(argv[argc])));
 		argc--;
 	}
-	sa(head);
+	tempo = head;
+	printf("9abla\n");
+	while (head)
+	{
+		printf("%d\n",head->num);
+		head = head->next;
+	}
+	printf("ba3da\n");
+	// sa(&tempo);
+	ra(&tempo);
 }
